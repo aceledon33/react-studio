dc8d1cf5c76d024c74a7936b8ad31583687bf344
aceledon33 <alexandra_celedon@brown.edu>
2022-11-10 17:27:49 -0500

    done lab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem"
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -10,20 +11,32 @@ bakeryData.forEach((item) => {
 
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
+  const [cart, setCart] = useState({});
+  const [total, setTotal] = useState(0);
   /* add your cart state code here */
 
+  function handleClick() {
+    console.log(cart)
+    console.log(total)
+  };
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+          <BakeryItem{...item} cart={cart} setCart={setCart} total={total} setTotal={setTotal}/>
       ))}
-
+      
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {
+        Object.keys(cart).map((key, index) => ( 
+          <div>{cart[key]}x {key}</div>
+        ))
+      }
+      <h3>Total: ${total}</h3>
       </div>
+      <div><button onClick={handleClick}>Test</button></div>
     </div>
   );
 }
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,34 @@
+import React, {useState} from 'react';
+import "../App.css";
 // TODO: create a component that displays a single bakery item
+
+//name, description, price (int), image
+export default function BakeryItem(props) {
+
+    function handleClick() {
+        props.setCart(addToCart(props.cart, props.name));
+        props.setTotal(addTotal(props.total, props.price))
+      };
+
+    function addTotal(total, price){
+        return total += price
+    }
+
+    function addToCart(cart, name){
+        if(cart.hasOwnProperty(name)){
+            cart[name] += 1
+        }
+        else {
+            cart[name] = 1
+        }
+        return cart
+    }
+    return <div>
+        <h1>{props.name}</h1>
+        <p>{props.description}</p>
+        <h3>{props.price}</h3>
+        <img src={props.image}/>
+        <button onClick={handleClick}>Add to Cart</button>
+        </div>
+
+}
